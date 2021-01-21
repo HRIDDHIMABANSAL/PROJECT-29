3f1b80299567cef36a6c41a07eb025509d387d45
HRIDDHIMABANSAL <71069219+HRIDDHIMABANSAL@users.noreply.github.com>
2021-01-21 19:43:32 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.1,
+            length: 0.05
+        }
+        this.bodyA = bodyA;
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+        var sling = this.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke("turquoise");
+        line(sling.x, sling.y, pointB.x, pointB.y);
+        }
+        }
+    
+}
